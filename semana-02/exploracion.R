4c4df0d90ecbffc3671c0f3fa1210e5382cf04a0
jgomezm-tecsup <jgomezm@tecsup.edu.pe>
2019-09-21 16:13:03 -0500

    Semana 02

--- semana-02/exploracion.R
+++ semana-02/exploracion.R
@@ -1,8 +1,8 @@
-# Instalación de Paquetes
+# Instalaci?n de Paquetes
 install.packages(c("gmodels", "ggplot2", "vcd","agricolae","tidyverse", "Hmisc", "psych", "fBasics", "treemap", "ggmap", "mosaic"),
                  dependencies = c("Depends"))
 #                 dependencies = c("Depends", "Suggests"))
-
+ 
 #########################################################
 # Ejemplo: Marketing Directo                          #
 #########################################################
@@ -20,7 +20,7 @@ DMark$Historial = factor(DMark$Historial,levels = c("Bajo","Medio","Alto"),order
 str(DMark)
 
 #------------------------------------------------------------------#
-#  a) Representación de Datos Cualitativos                         #
+#  a) Representaci?n de Datos Cualitativos                         #
 #------------------------------------------------------------------#
 
 # Tabla de Frecuencia
@@ -39,12 +39,12 @@ library(gmodels)
 CrossTable(DMark$Edad, format="SAS")
 CrossTable(DMark$Edad, format="SPSS")
 
-# Visualización de la Distribución de la Variable
+# Visualizaci?n de la Distribuci?n de la Variable
 # ------------------------------------------------
 
-# Gráfico de Barras
+# Gr?fico de Barras
 # -------------------
-barplot(pi, main="Distribución de las edades de los clientes", 
+barplot(pi, main="Distribuci?n de las edades de los clientes", 
         xlab="Grupo Etario", col = 1,
         ylab="Porcentaje de Clientes")
 
@@ -53,21 +53,21 @@ library(ggplot2)
 ggplot(data = DMark, aes(x = Edad, y = ..count.., fill = Edad)) +
   geom_bar() +
   scale_fill_manual(values = c("gray50", "orangered2","black")) +
-  labs(title = "Distribución de las edades de los clientes") +
+  labs(title = "Distribuci?n de las edades de los clientes") +
   theme_bw() +
   theme(legend.position = "bottom")
 
 
 
-# Gráfico de Sectores Circulares
+# Gr?fico de Sectores Circulares
 # ------------------------------
-pie(pi, main="Distribución de la Edad de los Clientes")
+pie(pi, main="Distribuci?n de la Edad de los Clientes")
 
 ## Colocar porcentajes
 lbls1 <- paste(names(table(DMark$Edad)), "\n",
                prop.table(table(DMark$Edad))*100,"%", sep="")
 pie(pi, labels = lbls1,
-	main="Distribución de la Edad de los Clientes")
+	main="Distribuci?n de la Edad de los Clientes")
 
 
 # Usando ggplot2
@@ -79,7 +79,7 @@ pie = pie + coord_polar("y", start=0) +
 pie = pie + scale_fill_manual(values=c("#55DDE0", "#F6AE2D", "#F26419", "#999999")) #Escala de colores
 pie = pie + labs(x = NULL, y = "% de clientes", 
                            fill = "Grupo Etario",
-                          title = "Distribución del Grupo Etario") # Remover y añadir etiquetas/título 
+                          title = "Distribuci?n del Grupo Etario") # Remover y a?adir etiquetas/t?tulo 
 pie = pie + theme_classic() + theme(axis.line = element_blank(),
                                     axis.text = element_blank(),
                                     axis.ticks = element_blank(),
@@ -89,11 +89,11 @@ pie
 # Dot Plots
 # ---------
 dotchart(prop.table(table(DMark$Edad))*100,
-         main="Distribución de la Edad de los Clientes", 
+         main="Distribuci?n de la Edad de los Clientes", 
          xlab="% de Clientes")
 
 ##___________________________________________________________________________________
-## Ejercicio: Analice la distribución del  Historial de Compra del cliente
+## Ejercicio: Analice la distribuci?n del  Historial de Compra del cliente
 ##___________________________________________________________________________________
 
 #------------------------------------------------------------------#
@@ -105,7 +105,7 @@ tabla1
 CrossTable(x = DMark$Edad,y = DMark$Historial)
 
 #------------------------------------------------------------------#
-#  c) Distribución condicional                                     #
+#  c) Distribuci?n condicional                                     #
 #------------------------------------------------------------------#
 tabla2=prop.table(tabla1,margin=1)
 tabla2
@@ -113,8 +113,8 @@ tabla2
 # Barras agrupadas
 barplot(t(tabla2),col=2:4,beside = T,
         xlab="Grupo Etario",
-        ylab="Proporción de Clientes",
-        main="Distribución del historial de compra según grupo etario")
+        ylab="Proporci?n de Clientes",
+        main="Distribuci?n del historial de compra seg?n grupo etario")
 legend("topright",legend=levels(DMark$Historial),col=2:4,
        pch=15,title="Historial de Compra")
 
@@ -122,28 +122,28 @@ legend("topright",legend=levels(DMark$Historial),col=2:4,
 par(mar=c(5.1, 4.1, 4.1, 8.1), xpd=TRUE) #Adicionar espacio extra
 barplot(t(tabla2),col=2:4,
         xlab="Grupo Etario",
-        ylab="Proporción de Clientes",
-        main="Distribución del historial de compra según grupo etario")
+        ylab="Proporci?n de Clientes",
+        main="Distribuci?n del historial de compra seg?n grupo etario")
 legend("topright", inset=c(-0.27,0),legend=levels(DMark$Historial),col=2:4,
        pch=15,title="Historial de Compra")
 
 #Usando ggplot2
 ggplot(data = na.omit(DMark), aes(x = Edad, y = ..count.., fill = Historial)) +
   geom_bar(position = "fill") +
-  labs(y = "% de clientes", title = "Distribución del historial de compra según grupo etario") +
+  labs(y = "% de clientes", title = "Distribuci?n del historial de compra seg?n grupo etario") +
   theme_bw() +
   theme(legend.position = "bottom")
 
 library(vcd)
 mosaicplot(~ Edad+Historial, data = DMark, color = 2:4, 
-           main="Distribución del historial de compra según grupo etario")
+           main="Distribuci?n del historial de compra seg?n grupo etario")
 
 ##___________________________________________________________________________________
-## Ejercicio: Analice si existe asociación entre el historial de compra y el género
+## Ejercicio: Analice si existe asociaci?n entre el historial de compra y el g?nero
 ##___________________________________________________________________________________
 
 #------------------------------------------------------------------#
-#  d) Representación de Datos Cuantitativos Discretos              #
+#  d) Representaci?n de Datos Cuantitativos Discretos              #
 #------------------------------------------------------------------#
 # Tabla de Frecuencias
 ni<-table(DMark$Hijos)
@@ -152,13 +152,13 @@ pi<-prop.table(table(DMark$Hijos))*100
 hijos.tabla<-t(rbind(ni,fi,pi))
 hijos.tabla
 
-# Visualización de la Distribución de la Variable
+# Visualizaci?n de la Distribuci?n de la Variable
 # ------------------------------------------------
-#Gráfico de Varas
+#Gr?fico de Varas
 plot(pi, type="h", lwd=2,
-     xlab="Número de hijos",
+     xlab="N?mero de hijos",
      ylab="Porcentaje de clientes",
-     main="Distribución del número de hijos por cliente")
+     main="Distribuci?n del n?mero de hijos por cliente")
 points(x =as.numeric(row.names(pi)),
        y =as.numeric(pi),
        pch=19,cex=1.5)
@@ -168,10 +168,10 @@ df <- as.data.frame(pi)
 ggplot(df, aes(x=Var1, y=Freq)) +
   geom_segment( aes(x=Var1, xend=Var1, y=0, yend=Freq)) +
   geom_point( size=5, color="red", fill=alpha("orange", 0.3), alpha=0.7, shape=21, stroke=2) +
-  labs(x = "# de hijos", y = "% de clientes", title = "Distribución del número de hijos por cliente") 
+  labs(x = "# de hijos", y = "% de clientes", title = "Distribuci?n del n?mero de hijos por cliente") 
 
 #------------------------------------------------------------------#
-#  e) Representación de Datos Cuantitativos Continuos              #
+#  e) Representaci?n de Datos Cuantitativos Continuos              #
 #------------------------------------------------------------------#
 
 # Tabla de Frecuencias (usando la regla de Sturges)
@@ -187,22 +187,22 @@ xout <- transform(xout,
 )
 xout
 
-# Obtener la tabla de una librería
+# Obtener la tabla de una librer?a
 library(agricolae)
 (table.freq(hist(DMark$Monto,breaks = "Sturges",plot=FALSE))) #Regla Sturges
 (table.freq(hist(DMark$Monto,breaks = "Scott",plot=FALSE)))   #Regla de Scott
 (table.freq(hist(DMark$Monto,breaks = "FD",plot=FALSE)))      #Regla de Friedman-Diaconis
 (table.freq(graph.freq(DMark$Monto,plot=FALSE)))   #Regla Sturges (Agricolae)
 
-# Visualización de la Distribución de la Variable
+# Visualizaci?n de la Distribuci?n de la Variable
 # ------------------------------------------------
-# Histograma y polígono de frecuencia
+# Histograma y pol?gono de frecuencia
 h1<-hist(DMark$Monto,breaks = "Sturges",
          xlab="Monto",
-         ylab="Número de clientes")
+         ylab="N?mero de clientes")
 polygon.freq(h1,frequency=1,col="red")
 
-# Polígono de Frecuencias (solo)
+# Pol?gono de Frecuencias (solo)
 h1<-hist(DMark$Monto,border=FALSE)
 polygon.freq(h1,frequency=1,col="red")
 
@@ -213,7 +213,7 @@ ggplot(data=DMark, aes(Monto)) +
                  fill="black", 
                  alpha = .75) + 
   geom_density(col=2) + 
-  labs(title="Monto de Crédito") +
+  labs(title="Monto de Cr?dito") +
   labs(x="Monto", y="# de clientes")+
   xlim(c(0, 6300)) 
 
@@ -221,7 +221,7 @@ ggplot(data=DMark, aes(Monto)) +
 hist(DMark$Monto,prob=TRUE)
 lines(density(DMark$Monto))
 
-#Gráfico de Densidad
+#Gr?fico de Densidad
 plot(density(DMark$Monto))
 
 # Usando ggplot2
@@ -245,35 +245,35 @@ points<-ogive.freq(h,col="red",frame=FALSE)
 print(points)
 
 ##___________________________________________________________________________________
-## Ejercicio: Analice la distribución del salario de los clientes
+## Ejercicio: Analice la distribuci?n del salario de los clientes
 ##___________________________________________________________________________________
 
 #------------------------------------------------------------------#
-#  f) Análisis descriptivo                                         #
+#  f) An?lisis descriptivo                                         #
 #------------------------------------------------------------------#
 
-# Resumen básico
+# Resumen b?sico
 summary(DMark)
 summary(DMark$Monto)
 
-# Definir funciones con medidas estadísticas
+# Definir funciones con medidas estad?sticas
 
-# Función para calcular CV
+# Funci?n para calcular CV
 CV <- function(x){
   (sd(x)/mean(x))*100
 }
 
-# Función para calcular asimetria (Pearson)
+# Funci?n para calcular asimetria (Pearson)
 A3 <- function(x){
   3*(mean(x)-median(x))/sd(x)
 }
 
-# Función para calcular el rango
+# Funci?n para calcular el rango
 rango <- function(x){
   diff(range(x))
 }
 
-# Función para calcular el rango intercuartílico
+# Funci?n para calcular el rango intercuart?lico
 RIC <- function(x){
   quantile(x,probs = 0.75,type = 6)-quantile(x,probs = 0.25,type = 6)
 }
@@ -307,7 +307,7 @@ DMark %>% summarise(Media = mean(Monto),
                     Asimetria = A3(Monto)
 )
 
-# Funciones de resumen de datos disponibles en otras librerías
+# Funciones de resumen de datos disponibles en otras librer?as
 Hmisc::describe(DMark$Monto)
 library(psych)
 psych::describe(DMark$Monto)
@@ -319,11 +319,11 @@ skewness(DMark$Monto)
 kurtosis(DMark$Monto)
 
 ##___________________________________________________________________________________
-## Ejercicio: Realice el análisis descriptivo para el salario de los clientes
+## Ejercicio: Realice el an?lisis descriptivo para el salario de los clientes
 ##___________________________________________________________________________________
 
 #------------------------------------------------------------------#
-#  g) Análisis descriptivo comparativo                             #
+#  g) An?lisis descriptivo comparativo                             #
 #------------------------------------------------------------------#
 
 #Usando funciones apply
@@ -356,10 +356,10 @@ DMark %>% group_by(Ecivil) %>%
                     Asimetria = A3(Monto)
 )
 
-# Usando librerías
+# Usando librer?as
 psych::describeBy(x = DMark$Monto, group = DMark$Ecivil)
 
-# Análisis comparativo usando visualización de datos
+# An?lisis comparativo usando visualizaci?n de datos
 # ---------------------------------------------------
 
 boxplot(DMark$Monto ~ DMark$Ecivil,
@@ -378,17 +378,17 @@ ggplot(data = DMark, aes(x = Monto, fill = Ecivil)) +
   theme_bw()
 
 ##___________________________________________________________________________________
-## Ejercicio: Realice un análisis comparativo de los gastos de los clientes por
+## Ejercicio: Realice un an?lisis comparativo de los gastos de los clientes por
 ##___________________________________________________________________________________
 
 #------------------------------------------------------------------#
-# h)  Asociación                                                     #
+# h)  Asociaci?n                                                     #
 #------------------------------------------------------------------#
 
 plot(DMark$Salario,DMark$Monto)
 cor(DMark$Salario,DMark$Monto)
 
-# Matriz de Diagramas de dispersión
+# Matriz de Diagramas de dispersi?n
 pairs(~Salario + Monto + Hijos + Catalogos,data=DMark)
 cor(DMark[,c(6,7,9,10)])
 corr.test(DMark[,c(6,7,9,10)])
@@ -398,7 +398,7 @@ cor.plot(cor(DMark[,c(6,7,9,10)]))
 #   Graficos especiales                                            #
 #------------------------------------------------------------------#
 
-# Gráfico de Redes
+# Gr?fico de Redes
 library(igraph)
 ebay.df <- read.csv("eBayNetwork.csv")
 
@@ -418,7 +418,7 @@ plot(g, vertex.label = NA, vertex.color = ifelse(isBuyer, "gray", "black"),
 library(treemap)
 tree.df <- read.csv("EbayTreemap.csv")
 
-# Añadir una columna con el feedback negativo
+# A?adir una columna con el feedback negativo
 tree.df$negative.feedback <- 1* (tree.df$Seller.Feedback < 0)
 
 # Dibujar el treemap
